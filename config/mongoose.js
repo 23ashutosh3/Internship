c4105904ccf6259d54ede93857d468690337a37d
23ashutosh3 <1ashu7200@gmail.com>
2020-12-08 16:39:55 +0530

    user account

--- config/mongoose.js
+++ config/mongoose.js
@@ -5,9 +5,7 @@ mongoose.connect('mongodb://localhost/codeial_development');
 const db = mongoose.connection;
 
 db.on('error', console.error.bind(console, "Error connecting to MongoDB"));
-
-
-db.once('open', function(){
+db.once('open', function() {
     console.log('Connected to Database :: MongoDB');
 });
 
